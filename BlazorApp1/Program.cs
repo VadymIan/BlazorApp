f5bdcbfa013abedca02099243ac7c264a5278f46
Vadym_Ianchenko <ia_vadim@ukr.net>
2022-08-10 12:40:05 +0300

    cdcd

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
